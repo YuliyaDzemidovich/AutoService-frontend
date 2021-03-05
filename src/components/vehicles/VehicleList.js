a563034b379a4c9064c2389905583c4071b3fd7a
Yuliya Dzemidovich <yuliya.dzemidovich@gmail.com>
2021-03-05 17:28:18 +0300

    implemented table row selection by id with styles change

--- src/components/vehicles/VehicleList.js
+++ src/components/vehicles/VehicleList.js
@@ -5,13 +5,17 @@ import { ButtonDelete } from "../ButtonDelete";
 import {styles} from "../../App.css";
 
 import ButtonContainer from "./ButtonContainer";
+import ReactTable from "react-table";
 
 export default class VehicleList extends Component {
     constructor(props) {
         super(props);
         this.state = {
             vehicles: [],
-            message: null
+            message: null,
+            rowEdit: null,
+            selectedRowIndex: [],
+            isTableRowSelected: false
         }
         this.refreshProducts = this.refreshProducts.bind(this);
     }
@@ -25,6 +29,23 @@ export default class VehicleList extends Component {
                     this.setState({vehicles: response.data})
                 }
             )
+    }
+    selectRow (id, e) {
+        e.preventDefault();
+        // selection uncheck - the same row was clicked
+        if  (this.state.isTableRowSelected && this.state.selectedRowIndex === id) {
+            this.setState({selectedRowIndex: this.state.selectedRowIndex = null,
+                            isTableRowSelected: this.state.isTableRowSelected = false});
+        }
+        // select a different row - another row was selected
+        else if  (this.state.isTableRowSelected && this.state.selectedRowIndex !== id) {
+            this.setState({selectedRowIndex: this.state.selectedRowIndex = id});
+        // select a row - no row was selected
+        } else {
+            this.setState({selectedRowIndex: this.state.selectedRowIndex = id,
+                            isTableRowSelected: this.state.isTableRowSelected = true});
+        }
+        //console.log(this.state.selectedRowIndex);
     }
     render() {
         return (
@@ -51,7 +72,8 @@ export default class VehicleList extends Component {
                                 {
                                     this.state.vehicles.map(
                                         vehicle =>
-                                            <tr key={vehicle.id}>
+                                            <tr key={vehicle.id} onClick={(e) => this.selectRow(vehicle.id, e)} 
+                                                className={this.state.selectedRowIndex == vehicle.id ? "selected-row" : "" }>
                                                 <td>{vehicle.model.brand.name}</td>
                                                 <td>{vehicle.model.name}</td>
                                                 <td>{vehicle.dateOfProduction}</td>
